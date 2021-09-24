2dff976fc8f072cc07da8573334800c60833edd7
Luca Martinelli <martinelliluca98@gmail.com>
2021-09-24 09:07:37 +0200

    Changed name

--- Freia/android/app/src/main/java/com/lucamartinelli/diana/MainActivity.java
+++ Freia/android/app/src/main/java/com/lucamartinelli/diana/MainActivity.java
@@ -1,4 +1,4 @@
-package com.freia;
+package com.lucamartinelli.diana;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Freia";
+    return "Diana";
   }
 }
--- Freia/ios/DianaTests/DianaTests.m
+++ Freia/ios/DianaTests/DianaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface FreiaTests : XCTestCase
+@interface DianaTests : XCTestCase
 
 @end
 
-@implementation FreiaTests
+@implementation DianaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
